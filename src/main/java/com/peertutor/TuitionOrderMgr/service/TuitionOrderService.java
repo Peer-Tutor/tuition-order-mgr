d1ff375b4d8bc4c9274cb2ed1ea4a90e42a34b65
DESKTOP-SDS0VIT\65834 <lyw415309@gmail.com>
2022-10-13 22:45:36 +0800

    Add logic to dealing with time conflict

--- src/main/java/com/peertutor/TuitionOrderMgr/service/TuitionOrderService.java
+++ src/main/java/com/peertutor/TuitionOrderMgr/service/TuitionOrderService.java
@@ -6,20 +6,18 @@ import com.peertutor.TuitionOrderMgr.repository.TuitionOrderRepository;
 import com.peertutor.TuitionOrderMgr.service.dto.TuitionOrderCriteria;
 import com.peertutor.TuitionOrderMgr.service.dto.TuitionOrderDTO;
 import com.peertutor.TuitionOrderMgr.service.mapper.TuitionOrderMapper;
-import io.github.jhipster.web.util.PaginationUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.sql.Date;
+import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 public class TuitionOrderService {
@@ -31,6 +29,8 @@ public class TuitionOrderService {
     private TuitionOrderRepository tuitionOrderRepository;
     @Autowired
     private TuitionOrderQueryService tuitionOrderQueryService;
+    @Autowired
+    private TutorCalendarService tutorCalendarService;
 
     public TuitionOrderService(TuitionOrderRepository tuitionOrderRepository, TuitionOrderMapper tuitionOrderMapper) {
         this.tuitionOrderRepository = tuitionOrderRepository;
@@ -43,23 +43,38 @@ public class TuitionOrderService {
     }
 
     public TuitionOrderDTO createTuitionOrder(TuitionOrderReq req) {
-        TuitionOrder tuitionOrder = tuitionOrderRepository.findByStudentIdAndTutorIdAndStartTimeAndEndTime(req.studentId, req.tutorId, req.startTime, req.endTime);
+        TuitionOrder tuitionOrder= new TuitionOrder();
 
-        if (tuitionOrder == null) {
-            tuitionOrder = new TuitionOrder();
+        if(req.id != null) {
+            tuitionOrderRepository.findById(req.id);
         }
 
-        tuitionOrder.setEndTime(req.endTime);
         tuitionOrder.setStatus(req.status);
         tuitionOrder.setStudentId(req.studentId);
         tuitionOrder.setTutorId(req.tutorId);
-        tuitionOrder.setStartTime(req.startTime);
+
+        Collections.sort(req.selectedDates);
+        List<Date> availableDates = tutorCalendarService.getTutorCalendar(req.name, req.sessionToken, req.tutorId);
+        String selectedDates = String.join(";", req.selectedDates.toString());
+
+        if (req.status != null && req.status != 2) {
+            if(!availableDates.containsAll(req.selectedDates)) {
+                return null;
+            }
+        }
+
+        tuitionOrder.setSelectedDates(selectedDates);
 
         try {
             tuitionOrder = tuitionOrderRepository.save(tuitionOrder);
         } catch (Exception e) {
             logger.error("TuitionOrder Profile Creation Failed: " + e.getMessage());
             return null;
+        } finally {
+            if (req.id != null && req.status == 1) {
+                removeConflictTuitionOrder(req.selectedDates);
+                tutorCalendarService.deleteTutorCalendar(req.name, req.sessionToken, req.tutorId, selectedDates);
+            }
         }
 
         TuitionOrderDTO result = tuitionOrderMapper.toDto(tuitionOrder);
@@ -76,6 +91,22 @@ public class TuitionOrderService {
         TuitionOrderDTO result = tuitionOrderMapper.toDto(tuitionOrder.get());
 
         return result;
+    }
+
+    public void removeConflictTuitionOrder(List<Date> selectedDates) {
+        List<String> dates = selectedDates.stream().map(date -> {return date.toString();}).collect(Collectors.toList());
+
+        dates.forEach(date -> {
+            List<TuitionOrder> orders = tuitionOrderRepository.findBySelectedDatesContainingAndStatus(date, 0);
+            orders.forEach(order -> {
+                try {
+                    order.setStatus(2);
+                    tuitionOrderRepository.save(order);
+                } catch (Exception e) {
+                    logger.error("TuitionOrder Profile Update Failed: " + e.getMessage());
+                }
+            });
+        });
     }
 
 }
